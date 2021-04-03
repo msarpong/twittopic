ae3c667bc1b8ca619212eefa92630d11798ce54e
msarpong <msarpong1991@gmail.com>
2021-04-03 19:04:42 +0200

    Initial commit

---
+++ app/src/main/java/org/msarpong/twittopic/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.msarpong.twittopic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
